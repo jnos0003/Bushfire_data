dce7bd4b1f29492319645b4e788137f3bcedb109
jnos0003 <jnos0003@student.monash.edu>
2024-09-24 17:03:26 +1000

    Delete vega_lite_vis.js

--- vega_lite_vis.js
+++
@@ -1,5 +0,0 @@
-var vg_1 = "bushfires_in_australia.vg.json";
-
-vegaEmbed("#map_aus", vg_1).then(function(result){
-  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
